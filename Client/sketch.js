7a617212bffe21f3cc090c2c07fff6b584ed6c79
Alex Dollar <Adollar@stcharlessd.org>
2020-10-26 22:03:30 -0500

    Updated Client

--- Client/sketch.js
+++ Client/sketch.js
@@ -12,18 +12,44 @@ function setup() {
 
 function draw() {
     background(127);
-    drawCard("1", createVector(0,0),100)
+    drawCard(playerData.StockCard, createVector(700,600),100, "Stock Pile");
+
+    drawCard(playerData.Discard1Card, createVector(200,600),100, "Discard Pile");
+    drawCard(playerData.Discard2Card, createVector(300,600),100, "Discard Pile");
+    drawCard(playerData.Discard3Card, createVector(400,600),100, "Discard Pile");
+    drawCard(playerData.Discard4Card, createVector(500,600),100, "Discard Pile");
+
+    drawCard(playerData.Build1Card, createVector(200,0),100, "Build Pile");
+    drawCard(playerData.Build2Card, createVector(300,0),100, "Build Pile");
+    drawCard(playerData.Build3Card, createVector(400,0),100, "Build Pile");
+    drawCard(playerData.Build4Card, createVector(500,0),100, "Build Pile");
+
+    drawCard(playerData.Hand1Card, createVector(150,300),100, "Hand");
+    drawCard(playerData.Hand2Card, createVector(250,300),100, "Hand");
+    drawCard(playerData.Hand3Card, createVector(350,300),100, "Hand");
+    drawCard(playerData.Hand4Card, createVector(450,300),100, "Hand");
+    drawCard(playerData.Hand5Card, createVector(550,300),100, "Hand");
 }
 
-function drawCard(cardText, pos, size) {
-    textAlign(CENTER, CENTER);
-    fill(255);
-    stroke(0);
-    rect(pos.x,pos.y,size,size*2);
-    fill(0)
-    text("* " + cardText + " *",pos.x,pos.y,size,size*2);
-    stroke(0,0,0,0);
-    textAlign(LEFT, TOP);
+function drawCard(cardText, pos, size, tooltip) {
+    if (cardText != null && cardText != undefined) {
+        textAlign(CENTER, CENTER);
+        fill(255);
+        stroke(0);
+        rect(pos.x,pos.y,size,size*2);
+        fill(0)
+        text("* " + cardText + " *",pos.x,pos.y,size,size*2);
+        stroke(0,0,0,0);
+        textAlign(LEFT, TOP);
+    } else {
+        fill(0,0,0,0);
+        stroke(0);
+        rect(pos.x, pos.y, size,size*2);
+        fill(0);
+        textAlign(CENTER, CENTER);
+        text(tooltip,pos.x,pos.y,size,size*2);
+        stroke(0,0,0,0);
+    }
 }
 
 function createWSConnection() {
@@ -61,7 +87,7 @@ function socketMessage(event) {
             data.return[command.name] = infoPrompt(command.info);
         }
         if (command.type === "SET") {
-            data.return[command.name] = set(command.name, command.value);
+            data.return[command.name] = setValue(command.name, command.value);
         }
     });
     data.type = type;
@@ -79,7 +105,8 @@ function infoPrompt(info) {
     return value
 }
 
-function set(key, value) {
+function setValue(key, value) {
+    console.log("Setting " + key + " To " + value);
     playerData[key] = value;
     return value;
 }
