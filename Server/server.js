b1edd7df5498b1ec8c765b4522dbe5c5a8943e32
Alex Dollar <adollar@stcharlessd.org>
2020-11-23 09:54:33 -0600

    Removed console logs and debug values

--- Server/server.js
+++ Server/server.js
@@ -268,7 +268,7 @@ server.on('connection', function(socket) {
                                 playerData[s].discard_pile_2 = [];
                                 playerData[s].discard_pile_3 = [];
                                 playerData[s].discard_pile_4 = [];
-                                playerData[s].stock_pile = popMultCard(draw_pile, 5);
+                                playerData[s].stock_pile = popMultCard(draw_pile, 30);
                                 authData[s].socket.send(JSON.stringify({
                                     return_type: null,
                                     clientKey: s,
@@ -466,7 +466,6 @@ server.on('connection', function(socket) {
                                 if (top_card == null || top_card == undefined || top_card == Number.NaN) top_card = "0";
                                 pushCard(translateDeckName(pop, object.clientKey),"~SB~".concat(parseInt(top_card) + 1));
                             }
-                            console.log(getTopCard(translateDeckName(pop, object.clientKey)));
                             if (getTopCard(translateDeckName(pop, object.clientKey)) === "RC") {
                                 let old = popCard(translateDeckName(pop, object.clientKey));
                                 let top_card = getTopCard(translateDeckName(push, object.clientKey));
@@ -475,13 +474,9 @@ server.on('connection', function(socket) {
                                 if ((top_card == null || top_card == undefined || top_card == Number.NaN || top_card === "0") && old === "RC") return;
                                 pushCard(translateDeckName(pop, object.clientKey),"~RC~".concat(parseInt(top_card) + -1));
                             }
-
-                            console.log(getTopCard(translateDeckName(pop, object.clientKey)));
-
                             if (CheckCardPlacement(translateDeckName(push, object.clientKey), getTopCard(translateDeckName(pop, object.clientKey)))) {
                                 pushCard(translateDeckName(push, object.clientKey), popCard(translateDeckName(pop, object.clientKey)));
                             }
-                            //if (getTopCard(translateDeckName(pop, object.clientKey)) == undefined && playerData[object.clientKey][translateDeckName(pop, object.clientKey)].length > 0) console.log("ERROR IN POP (maybe)");
                             if (pop === "StockCard" && playerData[object.clientKey].stock_pile.length <= 0) {
                                 turn_index = -1;
                                 keys.forEach(s => {
@@ -887,10 +882,6 @@ function countCards() {
         count = count + playerData[keys[i]].discard_pile_3.length;
         count = count + playerData[keys[i]].discard_pile_4.length;
         count = count + playerData[keys[i]].stock_pile.length;
-        console.log(playerData[keys[i]].discard_pile_1);
-        console.log(playerData[keys[i]].discard_pile_2);
-        console.log(playerData[keys[i]].discard_pile_3);
-        console.log(playerData[keys[i]].discard_pile_4);
         for (let j=0; j<5;j++) {
             if (playerData[keys[i]].hand[j] != null && playerData[keys[i]].hand[j] != undefined) {
                 count++;
@@ -899,9 +890,9 @@ function countCards() {
     }
     console.log("Card Amount: " + count);
     if (count != 162 && count != 180 && count != 324) console.log("CARD ERROR");
-    //console.log(draw_pile);
-    //console.log(build_pile_1);
-    //console.log(build_pile_2);
-    //console.log(build_pile_3);
-    //console.log(build_pile_4);
+    console.log(draw_pile);
+    console.log(build_pile_1);
+    console.log(build_pile_2);
+    console.log(build_pile_3);
+    console.log(build_pile_4);
 }
